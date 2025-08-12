c3b330599b05620d3f0b2ed611eecd8cbd6a64e6
dvcchamhocvcl <dvcchamhoc@gmail.com>
2025-08-12 14:23:29 +0700

    lab nodejs done

---
+++ Docker/nodejs_ubuntu_img/main.js
@@ -0,0 +1,13 @@
+// Import Express
+const express = require('express');
+const app = express();
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
